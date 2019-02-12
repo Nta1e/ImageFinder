8f93b486be18b59246f638c6df2dc8dfcdf56137
NtaleGrey <Shadikntale@gmail.com>
2019-02-12 15:34:46 +0300

    initial commit

---
+++ src/components/search/Search.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
+
+class Search extends Component {
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "11578351-9a99feb03afd0301f64b733f4",
+    images: []
+  };
+  onTextChange = e => {
+    const value = e.target.value;
+    this.setState({ [e.target.name]: value }, () => {
+      if (value === "") {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => err);
+      }
+    });
+  };
+  onAmountChange = (e, index, value) =>
+    this.setState({ amount: value }, () => this.state);
+  render() {
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search for Images"
+          fullWidth={true}
+        />
+        <br />
+        <SelectField
+          name="Amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+        <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default Search;
